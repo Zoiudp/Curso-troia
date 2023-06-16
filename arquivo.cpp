1808f2905b9bb5fead745f0839b0c033dd577b41
Rodrigo Marques Duarte <93834250+Zoiudp@users.noreply.github.com>
2023-06-16 19:54:01 -0300

    exercicio 1

--- arquivo.cpp
+++ arquivo.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(){
+    int idade;
+    cout << "Digite sua idade:";
+    cin >> idade;
+
+/*
+    if (idade >= 18){
+        cout << "Voce e maior de 18";
+    }else {
+        cout <<"Você é menor de idade";
+    }
+*/
+
+cout <<(idade >=18 ? "Voce e maior de 18 " : "Voce e menor de idade");
+
 
     return 0;
 }
